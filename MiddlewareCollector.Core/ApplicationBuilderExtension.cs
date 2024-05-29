8b11c1ab371ea8afadf6883932c4bc9373ab13ea
Ershad <ershadRaoufi@gmail.com>
2024-05-29 13:28:50 -0700

    Initial commit

---
+++ MiddlewareCollector.Core/ApplicationBuilderExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using MiddlewareCollector.Abstract;
+
+namespace MiddlewareCollector.Core
+{
+    public static class ApplicationBuilderExtension
+    {
+        public static void MiddlewareCollector(this IApplicationBuilder applicationBuilder)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        
+            var indicator = typeof(IMiddlewareCollector);
+        
+            var types = assemblies.SelectMany(x => x.DefinedTypes);
+            var serviceDiscoveries = types.Where(typeInfo => typeInfo.ImplementedInterfaces.Contains(indicator));
+
+            foreach (var typeInfo in serviceDiscoveries)
+            {
+                applicationBuilder.UseMiddleware(typeInfo);
+            }
+        }
+    }
+}
